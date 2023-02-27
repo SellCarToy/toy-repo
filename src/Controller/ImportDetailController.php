7fd2e3502371b42b3b4b9a9ac7d8337125a3033b
NQuVuong <vuongnqgcc210197@fpt.edu.vn>
2023-02-27 23:11:38 +0700

    show export order

--- src/Controller/ImportDetailController.php
+++ src/Controller/ImportDetailController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
-     * @Route("/detail")
+     * @Route("/imdetail")
      */
 class ImportDetailController extends AbstractController
 {
